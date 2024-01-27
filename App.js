495852aa77bc8a469ae23880d908fce620f8fdff
Kishore Kumar <kishorekumarcdckap@gmail.com>
2024-01-27 12:07:32 +0530

    name and age is updated

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   const [name , setName ]= useState('Jagadish')
+  const [age , setAge ]= useState('30')
   const [person , setPerson ]= useState({name:'Iron man' ,age :21});
 
   const btnClick =()=>{
@@ -12,11 +13,12 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <Text>My name is {name}</Text>
-      <Text>His name is {person.name} and his age is {person.age}</Text>
-      <View style ={styles.buttonContainer}>
-        <Button title='Update State' onPress={btnClick} />
-      </View>
+      <Text>Enter Name:</Text>
+      <TextInput placeholder='John Doe' style = {styles.input} onChangeText={(val)=> setName(val)}/>
+      <Text>Enter Age:</Text>
+
+      <TextInput placeholder='11' style = {styles.input} onChangeText={(val)=> setAge(val)}/> 
+      <Text>Name :{name} , age :{age}</Text>
     </View>
   );
 }
@@ -38,6 +40,15 @@ const styles = StyleSheet.create({
     color:'red',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input:{
+    borderWidth : 1,
+    borderColor:'#777',
+    padding: 8,
+    margin : 10,
+    width :200
+  
+  
   },
 
 });
