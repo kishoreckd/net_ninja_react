e2d538672a48b39f79711cd718712d57b27b3d4a
Kishore Kumar <kishorekumarcdckap@gmail.com>
2024-01-27 10:52:52 +0530

    text added

--- App.js
+++ App.js
@@ -4,8 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>React Native</Text>
-      <StatusBar style="auto" />
+
+      <View style ={styles.headers}>
+          <Text style ={styles.boldText}>Hello World</Text>
+      </View>
+      {/* <View style ={styles.body}>
+          <Text style ={styles.text}>Body</Text>
+          <Text style ={styles.text}>Body</Text>
+          <Text style ={styles.text}>Body</Text>
+          <Text style ={styles.text}>Body</Text>
+      </View> */}
     </View>
   );
 }
@@ -17,4 +25,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headers:{
+    backgroundColor:'pink', alignItems: 'center',
+    justifyContent: 'center',
+    padding:20
+  },
+  boldText: {
+    flex: 1,
+    color:'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
 });
