42cf58f70f243619ff55d8cb89bd5de0ffcc90ca
Kishore Kumar <kishorekumarcdckap@gmail.com>
2024-01-27 12:55:15 +0530

    flat list components

--- App.js
+++ App.js
@@ -1,38 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View , ScrollView } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View , ScrollView,FlatList } from 'react-native';
 
 export default function App() {
   
 const [people , setPeople] = useState ([
 
-  {name:'Kishore' ,key:'1'} ,
-  {name:'Akash' ,key:'2'} ,
-  {name:'Jeeva' ,key:'3'} ,
-  {name:'Dhanush' ,key:'4'} ,
-  {name:'Bala' ,key:'5'} ,
-  {name:'Bharat' ,key:'6'} ,
-  {name:'dkjhsidw' ,key:'7'} ,
-  {name:'jiihih' ,key:'8'} ,
-  {name:'hikhihi' ,key:'9'} ,
-  {name:'gfeifeg' ,key:'10'} ,
-  {name:'hikhdeiuhiihi' ,key:'11'} ,
+  {name:'Kishore' ,id:'1'} ,
+  {name:'Akash' ,id:'2'} ,
+  {name:'Jeeva' ,id:'3'} ,
+  {name:'Dhanush' ,id:'4'} ,
+  {name:'Bala' ,id:'5'} ,
+  {name:'Bharat' ,id:'6'} ,
+  {name:'dkjhsidw' ,id:'7'} ,
+  {name:'jiihih' ,id:'8'} ,
+  {name:'hikhihi' ,id:'9'} ,
+  {name:'gfeifeg' ,id:'10'} ,
+  {name:'hikhdeiuhiihi' ,id:'11'} ,
 ]);
   return (
     <View style={styles.container}>
 
-   <ScrollView>
+      <FlatList data={people}
+      numColumns={2}
+      keyExtractor={ (item) => item.id}
+      renderItem={({item})=>(
+        <Text style ={styles.item} >{item.name}</Text>
+      )}
+      
+      />
 
-     
+   {/* <ScrollView> 
    {people.map((item)=>{
 
-return (
-  <View key ={item.key}>
-    <Text style = {styles.item}> {item.name}</Text>
-     </View>
-)
-})}
-   </ScrollView>
+      return (
+        <View key ={item.key}>
+          <Text style = {styles.item}> {item.name}</Text>
+          </View>
+      )
+    })}
+   </ScrollView> */}
     </View>
   );
 }
@@ -51,6 +58,8 @@ const styles = StyleSheet.create({
     marginTop :24 ,
     padding :30 ,
     backgroundColor :'pink',
-    fontSize: 24
+    fontSize: 24,
+    marginHorizontal:10,
+    marginTop:24
   }
 });
