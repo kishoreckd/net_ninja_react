d1f8eeeb42810d3ca8c76c5b01c6b43021e3fcbd
Kishore Kumar <kishorekumarcdckap@gmail.com>
2024-01-29 11:54:30 +0530

    file added

--- App.js
+++ App.js
@@ -1,54 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View , ScrollView,FlatList , TouchableOpacity, Alert} from 'react-native';
-import Header from './components/header';
-import TodoItem from './components/todo';
-import AddTodo from './components/addTodo';
+
 
 export default function App() {
-const [todos ,setTodos] = useState([
-  {text: 'Buy a coffee' , key:'1'},
-  {text: 'Create an app' , key:'2'},
-  {text: 'play on the switch' , key:'3'},
-])
 
- const presshandler =(key)=>{
-  setTodos ((prevTodos)=>{
-    return prevTodos.filter(todo=>todo.key !=key)
-  })
- }
+ 
 
- const submitHandler =(text)=>{
-  if (text.length >3 ) {
-    setTodos((prevTodos)=>{
-      return [
-        {text:text, key:Math.random().toString()},
-        ...prevTodos
-      ]
-    });
-  }
-  else{
-    Alert.alert('OOPS!','Todos must be over 3 chars long',[
-      {text:'Understood', onPress:()=>console.log('alert closed')}
-    ])
-  }
- }
   return (
     <View style={styles.container}>
       {/**Header */}
-      <Header />
       <View style ={styles.content}>
       {/**To form */}
-      <AddTodo submitHandler = {submitHandler}/>
-      <View style = {styles.list}>
-        <FlatList
-        data = {todos}
-        renderItem={({ item }) => (
-          <TodoItem item={item} presshandler={presshandler} />
-
-        )}
-        />
-      </View>
+     
 
       </View>
      
@@ -63,13 +27,5 @@ const styles = StyleSheet.create({
     // paddingTop:40,
     // paddingHorizontal:20
   },
-  content : {
-    padding :40,
-    // backgroundColor:'pink',
-    flex:1
-  },
-  list:{
-    marginTop:20,
-    // backgroundColor:'yellow'
-  }
+  
 });
